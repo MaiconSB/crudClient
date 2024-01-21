8bb81facd1a978f57c1989890055b4f2b19710a4
Maicon Silveira <maiconsb2041@gmail.com>
2024-01-21 07:01:24 -0300

    DELETE Client

--- src/main/java/com/devsuperior/desafiocrudclientes/controllers/ClientController.java
+++ src/main/java/com/devsuperior/desafiocrudclientes/controllers/ClientController.java
@@ -3,6 +3,7 @@ package com.devsuperior.desafiocrudclientes.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -40,5 +41,10 @@ public class ClientController {
 	@PutMapping(value = "/{id}")
 	public ClientDTO update(@PathVariable Long id,@RequestBody ClientDTO dto) {
 		return service.update(id, dto);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public void delete(@PathVariable Long id) {
+		service.delete(id);
 	}
 }
